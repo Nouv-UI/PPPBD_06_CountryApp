90132f292101a469c683f34232c58418652898fa
nopalodon <nflcoding@gmail.com>
2026-09-16 10:03:41 +0700

    add spinner to mainActivity

--- app/src/main/java/com/example/countryapp/MainActivity.kt
+++ app/src/main/java/com/example/countryapp/MainActivity.kt
@@ -1,20 +1,50 @@
 package com.example.countryapp
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.ArrayAdapter
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.countryapp.databinding.ActivityMainBinding
+import java.util.zip.Inflater
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
     private lateinit var provinces: Array<String>
-
+    private val countries = arrayOf(
+        "indonesia",
+        "United States",
+        "United Kingdom",
+        "Germany",
+        "France",
+        "Australia",
+        "Japan",
+        "China",
+        "Brazil",
+        "Canada"
+    )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        provinces = resources.getStringArray(R.array.provinces)
+
+        with(binding) {
+            val adapterCountry = ArrayAdapter(this@MainActivity, android.R.layout.simple_spinner_item, countries)
+            adapterCountry.setDropDownViewResource(android.R.layout.simple_spinner_item)
+            spinnerCountry.adapter = adapterCountry
+
+            val adapterProvinces = ArrayAdapter(this@MainActivity, android.R.layout.simple_spinner_item, provinces)
+            adapterProvinces.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+            spinnerProvinces.adapter = adapterProvinces
+        }
+
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
